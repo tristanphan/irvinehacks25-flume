d52a41c2e6d169ec3a10de0338d1c849328778f2
Noakai Aronesty <noakai@gmail.com>
2025-01-25 15:55:17 -0800

    comment out for working version

--- server/api.py
+++ server/api.py
@@ -22,49 +22,50 @@ def get_nearest_10_h(lat, lon, danger_rad, listy, lat_field, lon_field):
     return near_10
 
 def get_nearest_10_cc(lat, lon, danger):
-    near_10_cc = []
-    api = overpy.Overpass()
+    pass
+#     near_10_cc = []
+#     api = overpy.Overpass()
     
-    query = f"""
-    (
-        area["name"="California"]->.searchArea;
-        node["amenity"="community_centre"](around:{5000},{lat},{lon});
-    );
-    out body;
-    """
-    result = api.query(query)
-    print(result)
-    encoded_query = urllib.parse.urlencode({'data': query})
+#     query = f"""
+#     (
+#         area["name"="California"]->.searchArea;
+#         node["amenity"="community_centre"](around:{5000},{lat},{lon});
+#     );
+#     out body;
+#     """
+#     result = api.query(query)
+#     print(result)
+#     encoded_query = urllib.parse.urlencode({'data': query})
     
-    url = "http://overpass-api.de/api/interpreter"
-    final_url = f"{url}?{encoded_query}"    
+#     url = "http://overpass-api.de/api/interpreter"
+#     final_url = f"{url}?{encoded_query}"    
 
-    with urllib.request.urlopen(final_url) as response:
-        data = json.loads(response.read().decode())
+#     with urllib.request.urlopen(final_url) as response:
+#         data = json.loads(response.read().decode())
 
-    for element in data['elements']:
-        name = element['tags'].get('name', 'Unknown')
-        cc_lat = element['lat']
-        cc_lon = element['lon']
-        dist = geodesic((lat, lon), (cc_lat, cc_lon)).miles
+#     for element in data['elements']:
+#         name = element['tags'].get('name', 'Unknown')
+#         cc_lat = element['lat']
+#         cc_lon = element['lon']
+#         dist = geodesic((lat, lon), (cc_lat, cc_lon)).miles
 
-        safe = True
-        if dist > danger: 
-            safe = True
-        else: 
-            safe = False
+#         safe = True
+#         if dist > danger: 
+#             safe = True
+#         else: 
+#             safe = False
 
-        if (name != "Unknown"):
-            cc_data = f'{{"Name": "{name}", "Distance": {dist}, "Longitude": {lon}, "Latitude": {lat}, "Safe": {safe}}}'
+#         if (name != "Unknown"):
+#             cc_data = f'{{"Name": "{name}", "Distance": {dist}, "Longitude": {lon}, "Latitude": {lat}, "Safe": {safe}}}'
         
-        if len(near_10_cc) < 10: 
-            insort(near_10_cc, cc_data, key=lambda x: x['Distance'])
-        elif dist < cc_data[9]['Distance']: 
-            cc_data.pop()
-            insort(near_10_cc, cc_data, key=lambda x: x['Distance'])
+#         if len(near_10_cc) < 10: 
+#             insort(near_10_cc, cc_data, key=lambda x: x['Distance'])
+#         elif dist < cc_data[9]['Distance']: 
+#             cc_data.pop()
+#             insort(near_10_cc, cc_data, key=lambda x: x['Distance'])
 
-    print(near_10_cc)
-    return near_10_cc
+#     print(near_10_cc)
+#     return near_10_cc
 
 def get_nearest_fires(lat, lon):
     fires_list = cal_fires_api()
